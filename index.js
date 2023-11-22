6dd8caf544d51e9eb97a109a717f0c4ee33634a5
nftpixels <weyers70@gmail.com>
2023-11-23 10:06:24 +1300

    Initial release
    Initial code push

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 3000;
+
+// Route to install openUPM
+app.get('/install-openupm', (req, res) => {
+  exec('npm install -g openupm-cli', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return res.status(500).json({ error: 'Failed to install openUPM' });
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+    res.json({ message: 'openUPM installed successfully' });
+  });
+});
+
+// Route to uninstall openUPM
+app.get('/uninstall-openupm', (req, res) => {
+  exec('npm uninstall -g openupm-cli', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return res.status(500).json({ error: 'Failed to uninstall openUPM' });
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+    res.json({ message: 'openUPM uninstalled successfully' });
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
